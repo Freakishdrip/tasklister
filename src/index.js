f140490396dea01aeeb7c5c0e8e5065ca4414c96
Nick Drummond <nckdrmmnd@gmail.com>
2023-12-12 15:38:13 -0700

    adsf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.querySelector("form").addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log(e)
+    console.log(e.target)
+    addNewToDo(e.target["new-task-description"].value)
 });
+
+function addNewToDo(form){
+    let p = document.createElement('p')
+    p.textContent = form
+    document.querySelector('#tasks').appendChild(p)
+}
+    
+})
+
+
+
+
+//take user input and add it to a unordered list with append child to the second div
+// text = form.getElementById('#new-task-description').value;
+//     console.log(text);
+//     form = document.createElement("ul")
+//     form.appendChild(form);
+//     document.getElementById("#tasks").appendChild(form);
